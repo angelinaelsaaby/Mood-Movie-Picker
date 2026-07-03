4822e6901e2064171eadc484aa1a150fd413ccad
angelinaelsaaby <angelinaelsaaby@gmail.com>
2026-07-03 15:23:09 +0530

    Add files via upload

---
+++ Movies.py
@@ -0,0 +1,54 @@
+moods = ["Happy","Sad","Thriller","Relaxed"]
+malmovies = {
+    "Happy": [
+        "Bangalore Days", "Premam", "Jacobinte Swargarajyam",
+        "Charlie", "Ustad Hotel", "Home", "Kilukkam",
+        "Nadodikkattu", "Pattanapravesam", "Godfather",
+        "Kunjiramayanam", "Aadu", "Aadu 2", "Om Shanti Oshana",
+        "Hridayam", "Varane Avashyamund", "Maheshinte Prathikaaram",
+        "Sudani from Nigeria", "Pranchiyettan and the Saint",
+        "Thanneer Mathan Dinangal", "Adi Kapyare Kootamani",
+        "Vettam", "Punjabi House", "CID Moosa",
+        "Chithram", "Meesa Madhavan", "Android Kunjappan",
+        "Classmates", "Minnal Murali", "Jan.E.Man"
+    ],
+
+    "Sad": [
+        "Thanmathra", "Kireedam", "Uyare",
+        "Amaram", "Dasharatham", "Bharatham",
+        "Thaniyavartanam", "Vatsalyam", "Pathemari",
+        "Take Off", "The Great Indian Kitchen", "Kaazhcha",
+        "Perumazhakkalam", "Aakashadoothu", "Karunam",
+        "Celluloid", "Spirit", "Munnariyippu",
+        "Indian Rupee", "Ayalum Njanum Thammil", "Ennu Ninte Moideen",
+        "Virus", "Kumbalangi Nights", "Rappakal",
+        "Adoor Gopalakrishnan’s Mathilukal", "Photographer",
+        "Ozhimuri", "Notebook", "Koode", "2018"
+    ],
+
+    "Thriller": [
+        "Drishyam", "Drishyam 2", "Anjaam Pathiraa",
+        "Mumbai Police", "Memories", "Traffic",
+        "Joseph", "Nayattu", "Ayyappanum Koshiyum",
+        "Manichithrathazhu", "Bhoothakaalam", "Forensic",
+        "Ela Veezha Poonchira", "Cold Case", "Kurup",
+        "Kishkindha Kaandam", "Rorschach", "Iratta",
+        "Kannur Squad", "Jana Gana Mana", "The Truth",
+        "Ee Thanutha Veluppan Kalathu", "Oppam", "Thondimuthalum Driksakshiyum",
+        "Ozler", "CBI Diary Kurippu", "Grandmaster",
+        "7th Day", "Ramaleela", "Take Off"
+    ],
+
+    "Relaxed": [
+        "Charlie", "Ustad Hotel", "Home",
+        "Bangalore Days", "Premam", "Om Shanti Oshana",
+        "Anandam", "Salt N Pepper", "Koode",
+        "Thanneer Mathan Dinangal", "Hridayam", "Jacobinte Swargarajyam",
+        "Kumbalangi Nights", "Sudani from Nigeria", "Maheshinte Prathikaaram",
+        "Njandukalude Naattil Oridavela", "1983", "Philips and the Monkey Pen",
+        "Rakshadhikari Baiju Oppu", "Sunday Holiday", "June",
+        "How Old Are You", "Bicycle Thieves", "Ayalum Njanum Thammil",
+        "Classmates", "Njan Prakashan", "Adi Kapyare Kootamani",
+        "Vikramadithyan", "Poomaram", "Jan.E.Man"
+    ]
+}
